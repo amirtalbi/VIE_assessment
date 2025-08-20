2d49360223e8fd1a27fcfa91668fa2d7c3cc9d21
amirtalbi <amirotalbi94@gmail.com>
2025-08-20 14:18:32 +0200

    feat: chargement dynamique selon client

--- index.php
+++ index.php
@@ -1,11 +1,44 @@
+<?php
+$clientId = $_COOKIE['clientId'] ?? 'clienta';
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tool4cars</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
+
 <body>
-    <div class="dynamic-div" data-module="cars" data-script="ajax"></div>
+    <button class="switch-client" data-client="clienta">Client A</button>
+    <button class="switch-client" data-client="clientb">Client B</button>
+    <button class="switch-client" data-client="clientc">Client C</button>
+
+    <div class="dynamic-div"
+        data-module="cars"
+        data-script="ajax">
+    </div>
+
+    <script>
+        function loadDynamicContent(clientId) {
+            var module = $(".dynamic-div").data("module");
+            var script = $(".dynamic-div").data("script");
+
+            var url = "/customs/" + clientId + "/modules/" + module + "/" + script + ".php";
+
+            $.get(url, function(response) {
+                $(".dynamic-div").html(response);
+            });
+        }
+
+        var currentClient = "<?php echo $clientId; ?>";
+        loadDynamicContent(currentClient);
+
+        $(".switch-client").click(function() {
+            var newClient = $(this).data("client");
+            document.cookie = "clientId=" + newClient;
+            loadDynamicContent(newClient);
+        });
+    </script>
 </body>
+
 </html>
